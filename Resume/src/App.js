ee068ed8e8dfa4669fb410f8c57322d02a651254
LeoTafarello <leonardo.tafarello@hotmail.com>
2021-01-27 12:18:22 -0300

    Add files via upload

---
+++ Resume/src/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import Routes from './routes';
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component{
+
+    render(){
+
+        return(
+            <div className='app'>
+               <Routes/>
+            </div>
+        )
+    }
+}
+
+export default App;
